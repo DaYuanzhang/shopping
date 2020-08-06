f09699f277af0f23bb25b5a5d3d9a8e5b4cc087c
ChenZheng <1641774518@qq.com>
2020-08-06 16:45:03 +0800

    版本0.80

--- db.sql
+++ db.sql
@@ -13,13 +13,23 @@ CREATE TABLE users (
   PRIMARY KEY (`id`)
 );
 insert into users(loginName,password,remark,Role,email,tel) values ('admin','admin','测试数据:管理员用户','管理员','1236523652@qq.com','15326323589');
-
+insert into users(loginName,password,remark,Role,email,tel) values ('chen','123456','测试数据:客户用户','客户','1523652489@qq.com','12356956848');
+insert into users(loginName,password,remark,Role,email,tel) values ('lin','123456','测试数据:商家用户','商家','1368945685@qq.com','17856359465');
+insert into users(loginName,password,remark,Role,email,tel) values ('jiang','123456','测试数据:客户用户','客户','1546258965@qq.com','13658974586');
+insert into users(loginName,password,remark,Role,email,tel) values ('he','123456','测试数据:客户用户','客户','1321454651@qq.com','16359845682');
+insert into users(loginName,password,remark,Role,email,tel) values ('zheng','123456','测试数据:客户用户','客户','1547586954@qq.com','13625123652');
+insert into users(loginName,password,remark,Role,email,tel) values ('kang','123456','测试数据:客户用户','客户','1523654758@qq.com','19953625248');
+insert into users(loginName,password,remark,Role,email,tel) values ('jun','123456','测试数据:客户用户','客户','15236958458@qq.com','19568958745');
+insert into users(loginName,password,remark,Role,email,tel) values ('zhao','123456','测试数据:商家用户','商家','1698546523@qq.com','11365889544');
+insert into users(loginName,password,remark,Role,email,tel) values ('qian','123456','测试数据:商家用户','商家','1475684547@qq.com','18455563236');
 
 # 分类表
 CREATE TABLE category (
 cid VARCHAR(32) PRIMARY KEY ,
 cname VARCHAR(50)
 );
+
+
 #商品表
 CREATE TABLE products(
 pid VARCHAR(32) PRIMARY KEY ,
@@ -42,7 +52,7 @@ mer_id INT(11),
 addr  VARCHAR(50),
 PRIMARY KEY(oid),
 CONSTRAINT oc_fk FOREIGN KEY(con_id) REFERENCES users(id),
-CONSTRAINT op_fk FOREIGN KEY(pro_id) REFERENCES products(pid)
+CONSTRAINT op_fk FOREIGN KEY(pro_id) REFERENCES products(pid),
 CONSTRAINT om_fk FOREIGN KEY(mer_id) REFERENCES users(id)
 );
 
@@ -53,18 +63,29 @@ CREATE TABLE request (
   `remark` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`req_number`)
 );
-INSERT INTO request (id, loginName) VALUES('1','admin');
+INSERT INTO request(id,loginName,remark) VALUES('2','chen','我要赚钱');
+INSERT INTO request(id,loginName,remark) VALUES('4','jiang','我有梦想');
+INSERT INTO request(id,loginName,remark) VALUES('5','he','我要当老板');
+
 
 INSERT INTO category(cid,cname) VALUES('c001','家电');
 INSERT INTO category(cid,cname) VALUES('c002','服饰');
 INSERT INTO category(cid,cname) VALUES('c003','化妆品');
 #商品
-INSERT INTO products(pid, pname,price,flag,category_id) VALUES('p1','联想',5000,'1','c001');
-INSERT INTO products(pid, pname,price,flag,category_id) VALUES('p2','海尔',3000,'1','c001');
-INSERT INTO products(pid, pname,price,flag,category_id) VALUES('p3','雷神',5000,'1','c001');
-INSERT INTO products (pid, pname,price,flag,category_id) VALUES('p4','JACKJONES',800,'1','c002');
-INSERT INTO products (pid, pname,price,flag,category_id) VALUES('p5','真维斯',200,'1','c002');
-INSERT INTO products (pid, pname,price,flag,category_id) VALUES('p6','花花公子',440,'1','c002');
-INSERT INTO products (pid, pname,price,flag,category_id) VALUES('p7','劲霸',2000,'1','c002');
-INSERT INTO products (pid, pname,price,flag,category_id) VALUES('p8','香奈儿',800,'1','c003');
-INSERT INTO products (pid, pname,price,flag,category_id) VALUES('p9','相宜本草',200,'1','c003');
+
+INSERT INTO products(pid, pname,price,flag,category_id,s_id,inventory) VALUES('p1','联想',5000,'1','c001',3,60);
+INSERT INTO products(pid, pname,price,flag,category_id,s_id,inventory) VALUES('p2','海尔',3000,'1','c001',3,35);
+INSERT INTO products(pid, pname,price,flag,category_id,s_id,inventory) VALUES('p3','雷神',5000,'1','c001',3,21);
+INSERT INTO products (pid, pname,price,flag,category_id,s_id,inventory) VALUES('p4','JACKJONES',800,'1','c002',9,13);
+INSERT INTO products (pid, pname,price,flag,category_id,s_id,inventory) VALUES('p5','真维斯',200,'1','c002',9,125);
+INSERT INTO products (pid, pname,price,flag,category_id,s_id,inventory) VALUES('p6','花花公子',440,'1','c002',9,365);
+INSERT INTO products (pid, pname,price,flag,category_id,s_id,inventory) VALUES('p7','劲霸',2000,'1','c002',9,1000);
+INSERT INTO products (pid, pname,price,flag,category_id,s_id,inventory) VALUES('p8','香奈儿',800,'1','c003',10,536);
+INSERT INTO products (pid, pname,price,flag,category_id,s_id,inventory) VALUES('p9','相宜本草',200,'1','c003',10,152);
+
+#订单
+INSERT INTO orders(con_id,pro_id,dat,mer_id,addr) VALUES(2,'p1','2020-12-13',3,'ddasdasdas');
+INSERT INTO orders(con_id,pro_id,dat,mer_id,addr) VALUES(2,'p2','2020-09-01',9,'fgvkjhbk.noih');
+INSERT INTO orders(con_id,pro_id,dat,mer_id,addr) VALUES(2,'p3','2020-10-04',10,'asfasgassdasda');
+INSERT INTO orders(con_id,pro_id,dat,mer_id,addr) VALUES(5,'p6','2019-12-05',10,'45345655465466fsa');
+INSERT INTO orders(con_id,pro_id,dat,mer_id,addr) VALUES(5,'p9','2019-11-02',3,'345dsfas');
